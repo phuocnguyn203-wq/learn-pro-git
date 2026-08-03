38953dc8b2316795d056dc8c3e7c2ccf7ef65617
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-08-03 11:56:19 +0700

    add fun fact for server

--- greet.py
+++ greet.py
@@ -3,6 +3,7 @@ def greet(name):
 
 def main():
     greet('John')
+    print('Fun fact: You can\'t cache if your response is 3xx')
     print(f'This is server. You can play supergame')
 
 if __name__ == '__main__':

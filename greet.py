fd3998f423714187aac3bf87273aef4623f445d7
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-08-03 11:59:33 +0700

    Introduce what a server capable of

--- greet.py
+++ greet.py
@@ -5,6 +5,8 @@ def main():
     greet('John')
     print('Fun fact: You can\'t cache if your response is 3xx')
     print(f'This is server. You can play supergame')
+    print('It can also be used for AI training, it has 30 SK593\
+        Traning trillion tokens in term of second')
 
 if __name__ == '__main__':
     main()
